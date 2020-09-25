2bf9ab50c695275ce959a77f43012e2b25f6d274
Leonardo Gomes <leonardo.gdsilva@amedigital.com>
2020-09-24 22:21:01 -0300

    Create LessonService

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Lesson } from './lesson.entity';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(
+    name: string,
+    startDate: string,
+    endDate: string,
+  ): Promise<Lesson> {
+    const lesson: Lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
+}
