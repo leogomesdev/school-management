bdd5359dba61c27abbb1b8a4c2a323cc54868065
Leonardo Gomes <leonardo.gdsilva@amedigital.com>
2020-09-24 21:55:24 -0300

    Setup database connection (mongodb)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Lesson {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
