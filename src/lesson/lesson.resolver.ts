6ca9ae575c518dcc61fb90ce06fb6284900a8c65
Leonardo Gomes <leonardo.gdsilva@amedigital.com>
2020-09-29 20:03:46 -0300

    Creates lesson Query

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -8,13 +8,8 @@ export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
   @Query(() => LessonType)
-  lesson(): LessonType {
-    return {
-      id: 'xpto',
-      name: 'Physics class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string): Promise<Lesson> {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation(() => LessonType)
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -24,4 +24,8 @@ export class LessonService {
 
     return this.lessonRepository.save(lesson);
   }
+
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
 }
