6bd0fc4a2f5d47e2cc3250e3eabfd7904f07fc24
Leonardo Gomes <leonardo.gdsilva@amedigital.com>
2020-09-29 15:23:58 -0300

    Implements createLesson() in LessonResolver

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,12 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { Lesson } from './lesson.entity';
+import { LessonService } from './lesson.service';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query(() => LessonType)
   lesson(): LessonType {
     return {
@@ -15,7 +18,11 @@ export class LessonResolver {
   }
 
   @Mutation(() => LessonType)
-  createLesson(): any {
-    return new Lesson();
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
